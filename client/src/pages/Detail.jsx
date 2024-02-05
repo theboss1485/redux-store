e4d6775d1fd7502bb4685d06fd4221ec5b5c422c
DOOM Guy 1485 <theboss1485@gmail.com>
2024-02-05 16:06:56 -0500

    Reinstalled client-side dependencies to try to fix heroku

--- client/src/pages/Detail.jsx
+++ client/src/pages/Detail.jsx
@@ -3,15 +3,6 @@ import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
-// import {
-
-//     REMOVE_FROM_CART,
-//     UPDATE_CART_QUANTITY,
-//     ADD_TO_CART,
-//     UPDATE_PRODUCTS,
-
-//} from '../utils/actions';
 import {
 
     addToCart as addToCartAction,
